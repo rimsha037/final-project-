167ac814c3051af1b5294506490a1d17d08bc6ea
rimsha037 <84376917+rimsha037@users.noreply.github.com>
2021-06-04 10:22:56 -0700

    Update hash.py

--- hash.py
+++ hash.py
@@ -2,9 +2,6 @@ import uuid
 import hashlib
 import streamlit as st
 
-st.title("PASSWORD TO HASH CONVERTER")
-
-
 def hash_password(password):
    # uuid is used to generate a random number of the specified password
    salt = uuid.uuid4().hex
@@ -15,12 +12,25 @@ def check_password(hashed_password, user_password):
    return password == hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
 
 new_pass = st.text_input('Please enter a password: ')
+empty=""
+
+
 hashed_password = hash_password(new_pass)
-st.write('The string to store in the db is: ' + hashed_password)
+
+if new_pass==empty:
+   st.write("")
+else:
+   st.write('The string to store in the db is: ' + hashed_password)
 old_pass = st.text_input('Now please enter the password again to check: ')
+
 
 if check_password(hashed_password, old_pass):
    st.write('You entered the right password')
 else:
    st.write('Passwords do not match')
+
+
+
+
+
 

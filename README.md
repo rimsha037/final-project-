1408a0785642736c31e20aadee686f48f279a972
rimsha037 <84376917+rimsha037@users.noreply.github.com>
2021-06-04 04:06:05 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# final-project-
+# web app deployed on Heroku
+
+The deployed web app is live at https: https://scanner-xss.herokuapp.com/
+
+
+
+The web app was built in Python using the following libraries:
+* streamlit
+* beautifulsoup4
+* urljoin
+
+project by husnain shahid 
+thanks ❤
---
+++ hash.py
@@ -0,0 +1,22 @@
+import uuid
+import hashlib
+import streamlit as st
+
+def hash_password(password):
+   # uuid is used to generate a random number of the specified password
+   salt = uuid.uuid4().hex
+   return hashlib.sha256(salt.encode() + password.encode()).hexdigest() + ':' + salt
+
+def check_password(hashed_password, user_password):
+   password, salt = hashed_password.split(':')
+   return password == hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
+
+new_pass = st.text_input('Please enter a password: ')
+hashed_password = hash_password(new_pass)
+st.write('The string to store in the db is: ' + hashed_password)
+old_pass = st.text_input('Now please enter the password again to check: ')
+
+if check_password(hashed_password, old_pass):
+   st.write('You entered the right password')
+else:
+   st.write('Passwords do not match')

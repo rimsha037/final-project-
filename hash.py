bafa7829f7a594aed67db83f4ffc96abb5f355f7
rimsha037 <84376917+rimsha037@users.noreply.github.com>
2021-06-13 05:39:25 -0700

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,36 +1,45 @@
-import uuid
-import hashlib
 import streamlit as st
-
-def hash_password(password):
-   # uuid is used to generate a random number of the specified password
-   salt = uuid.uuid4().hex
-   return hashlib.sha256(salt.encode() + password.encode()).hexdigest() + ':' + salt
-
-def check_password(hashed_password, user_password):
-   password, salt = hashed_password.split(':')
-   return password == hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
-
-new_pass = st.text_input('Please enter a password: ')
-empty=""
+import re
+import hashlib
+from PIL import Image
 
 
-hashed_password = hash_password(new_pass)
-
-if new_pass==empty:
-   st.write("")
-else:
-   st.write('The string to store in the db is: ' + hashed_password)
-old_pass = st.text_input('Now please enter the password again to check: ')
+def make_hashes(password):
+    return hashlib.sha256(str.encode(password)).hexdigest()
 
 
-if check_password(hashed_password, old_pass):
-   st.write('You entered the right password')
-else:
-   st.write('Passwords do not match')
+def check_hashes(password, hashed_text):
+    if make_hashes(password) == hashed_text:
+        return hashed_text
+    return False
 
 
+def main():
+    st.header("-------------------Password Generator-------------------")
+    st.info("This APP will generate SHA256 Hashes ")
+    image = Image.open('download (1).jpg')
+    st.image(image, width=750)
+    st.subheader("This app will check the password is strong or not")
+
+    if st.checkbox("Check Your Password"):
+        image = Image.open('OIP (1).jpg')
+        st.image(image, width=750)
+        password = st.text_input("Password", type='password')
+        if st.button("submit"):
+            if (len(password) >= 8):
+                if bool(re.match('((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,30})', password)) == True:
+                    st.success("You entered Strong password")
+                    hashed_pswd = make_hashes(password)
+                    st.success("Hashes of your password is: ")
+                    st.info(hashed_pswd)
 
 
+                elif bool(re.match('((\d*)([a-z]*)([A-Z]*)([!@#$%^&*]*).{8,30})', password)) == True:
+                    st.warning("The password is weak!")
+                    st.warning("Your Password must contain Uppercase, Lowercase, Number and Special Characters")
+            else:
+                st.error("Password must contain at least 8 characters.")
 
 
+if __name__ == '__main__':
+    main()
